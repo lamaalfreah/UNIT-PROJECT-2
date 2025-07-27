d6d0b5e699abdb6285907ac754a8218b2b37f361
Lama Alfreah <lamaaalfreah@gmail.com>
2025-07-27 23:49:11 +0300

    Update views.py

--- Story/contact/views.py
+++ Story/contact/views.py
@@ -9,12 +9,8 @@ def contact_us(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('contact')
+            return redirect('/')
+
     else:
         form = ContactForm()
     return render(request, 'contact/contact.html', {'form': form})
-
-# View to display submitted contact messages
-def contact_messages(request):
-    messages = Contact.objects.order_by('-created_at')
-    return render(request, 'contact/messages.html', {'messages': messages})
