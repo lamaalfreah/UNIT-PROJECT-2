b77309f01a9ba471b04e613040960cf6f13df16f
Lama Alfreah <lamaaalfreah@gmail.com>
2025-07-28 00:00:02 +0300

    Update forms.py

---
+++ Story/stories/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Story, Comment
+
+class StoryForm(forms.ModelForm):
+    class Meta:
+        model = Story
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(StoryForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['full_name', 'content']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
