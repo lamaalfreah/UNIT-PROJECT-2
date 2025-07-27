12eace2e71a99b33abb0cdb90f6153a2aebe9b31
Lama Alfreah <lamaaalfreah@gmail.com>
2025-07-27 23:54:01 +0300

    Update views.py

--- Story/main/views.py
+++ Story/main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from stories.models import Story
+from contact.models import Contact
 
 # Create your views here.
 def home_view(request):
     latest_stories = Story.objects.order_by('-created_at')[:3] 
-    return render(request, 'main/home.html', {'latest_stories': latest_stories})
+    messages = Contact.objects.order_by('-created_at')
+    return render(request, 'main/home.html', {
+        'latest_stories': latest_stories,
+        'messages': messages
+    })
 
 def about_view(request):
     return render(request, 'main/about.html')
