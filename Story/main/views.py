b7d4b1a6d5c4b304bed06909f53950981507482f
Lama Alfreah <lamaaalfreah@gmail.com>
2025-07-28 23:05:30 +0300

    Update views.py

--- Story/main/views.py
+++ Story/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from stories.models import Story
 from contact.models import Contact
+from django.http import HttpRequest
 
 # View to render the homepage with the latest 3 stories and all user messages
-def home_view(request):
+def home_view(request:HttpRequest):
     latest_stories = Story.objects.order_by('-created_at')[:3] 
     messages = Contact.objects.order_by('-created_at')
     return render(request, 'main/home.html', {
@@ -12,5 +13,5 @@ def home_view(request):
     })
 
 # View to render the About Us page
-def about_view(request):
+def about_view(request:HttpRequest):
     return render(request, 'main/about.html')
