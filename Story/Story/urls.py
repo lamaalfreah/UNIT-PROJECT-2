168e4536f3ead7e3cc80f13483fa5b8d0c24c0f3
Lama Alfreah <lamaaalfreah@gmail.com>
2025-07-28 23:14:19 +0300

    Update urls.py

--- Story/Story/urls.py
+++ Story/Story/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('stories/', include('stories.urls')),
+    path('contact/', include("contact.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
