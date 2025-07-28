26c66e5afadc5eb31fea7b36cb21cc9bb6635c08
Lama Alfreah <lamaaalfreah@gmail.com>
2025-07-28 23:06:34 +0300

    Update views.py

--- Story/contact/views.py
+++ Story/contact/views.py
@@ -2,9 +2,10 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from .models import Contact
+from django.http import HttpRequest
 
 # Contact form view - handles submission and validation
-def contact_us(request):
+def contact_us(request:HttpRequest):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
